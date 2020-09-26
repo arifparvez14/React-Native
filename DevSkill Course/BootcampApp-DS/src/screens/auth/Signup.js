e38f1d96deab7ee147c8abc4383eb8973ad09d09
TheGenial <arifulislam14.ict@gmail.com>
2020-09-26 22:45:40 +0600

    API code and project code added.
    API code and project code added.

--- DevSkill Course/BootcampApp-DS/src/screens/auth/Signup.js
+++ DevSkill Course/BootcampApp-DS/src/screens/auth/Signup.js
@@ -7,7 +7,7 @@ import { Formik } from "formik";
 import * as yup from "yup";
 import TextInput from "../../components/common/Input";
 import Button from "../../components/common/Button";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import API from "../../api";
 
 const validationSchema = yup.object().shape({
@@ -28,75 +28,78 @@ const validationSchema = yup.object().shape({
 
 export default function Signup({ navigation }) {
   return (
-    <View style={cs.container}>
-      <Formik
-        initialValues={{ email: "", password: "", bio: "", name: "" }}
-        onSubmit={async (values, action) => {
-          console.log({ values });
-          const registerURL = "auth/register";
-          API.post(registerURL, values)
-            .then((res) => console.log(res))
-            .catch((err) => {
-              console.log(err);
-            });
-          // try {
-          //   let res = await API.post(registerURL, values);
-          //   console.log("res ", res);
-          // } catch (err) {
-          //   console.log("err ", err.response);
-          // }
-        }}
-        validationSchema={validationSchema}
-      >
-        {(formikProps) => {
-          return (
-            <View style={styles.formWrapper}>
-              <TextInput
-                placeholder="Full Name"
-                formikProps={formikProps}
-                formikKey={"name"}
-                autoCapitalize="words"
-              />
+    <ScrollView>
+      <View style={cs.container}>
+        <Formik
+          initialValues={{ email: "", password: "", bio: "", name: "" }}
+          onSubmit={async (values, action) => {
+            console.log({ values });
+            const registerURL = "auth/register";
+            API.post(registerURL, values)
+              .then((res) => console.log(res))
+              .catch((err) => {
+                console.log(err);
+              });
+            // try {
+            //   let res = await API.post(registerURL, values);
+            //   console.log("res ", res);
+            // } catch (err) {
+            //   console.log("err ", err.response);
+            // }
+          }}
+          validationSchema={validationSchema}
+        >
+          {(formikProps) => {
+            return (
+              <View style={styles.formWrapper}>
+                <TextInput
+                  placeholder="Full Name"
+                  formikProps={formikProps}
+                  formikKey={"name"}
+                  autoCapitalize="words"
+                />
 
-              <TextInput
-                placeholder="Email"
-                formikProps={formikProps}
-                formikKey={"email"}
-                autoCapitalize="none"
-              />
+                <TextInput
+                  placeholder="Email"
+                  formikProps={formikProps}
+                  formikKey={"email"}
+                  autoCapitalize="none"
+                />
 
-              <TextInput
-                placeholder="Password"
-                formikProps={formikProps}
-                formikKey={"password"}
-                secureTextEntry={true}
-              />
+                <TextInput
+                  placeholder="Password"
+                  formikProps={formikProps}
+                  formikKey={"password"}
+                  secureTextEntry={true}
+                />
 
-              <TextInput
-                placeholder="Short bio"
-                formikProps={formikProps}
-                formikKey={"bio"}
-              />
+                <TextInput
+                  placeholder="Short bio"
+                  formikProps={formikProps}
+                  formikKey={"bio"}
+                />
 
-              <Button
-                onPress={formikProps.handleSubmit}
-                style={{ margin: Metrics.doubleBase }}
-                title="Sign up"
-              />
-            </View>
-          );
-        }}
-      </Formik>
+                <Button
+                  onPress={formikProps.handleSubmit}
+                  style={{ margin: Metrics.doubleBase }}
+                  title="Sign up"
+                />
+              </View>
+            );
+          }}
+        </Formik>
 
-      <View style={styles.footer}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Text semiBold centered>
-            By continuing, you accept the <Text primaryColor>Terms of Use</Text>{" "}
-            and <Text primaryColor>Privacy Policy.</Text>
-          </Text>
-        </TouchableOpacity>
+        <View style={styles.footer}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Text semiBold centered>
+              By continuing, you accept the{" "}
+              <Text primaryColor>Terms of Use</Text> and{" "}
+              <Text primaryColor>Privacy Policy.</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
